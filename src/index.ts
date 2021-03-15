4fd70df924de2cb42dc18140bd3ffb302d93f53c
Ash Holland <ash@sorrel.sh>
2021-03-15 04:44:47 +0000

    Add React Developer Tools extension

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -23,7 +24,14 @@ const createWindow = (): void => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  installExtension(REACT_DEVELOPER_TOOLS).then(extensions => {
+    console.log(`Installed extensions: ${extensions}`);
+    createWindow();
+  }).catch(err => {
+    console.error(`Failed to install extensions: ${err}`);
+  })
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
