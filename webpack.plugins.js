42fe3ba259b2b0ac9b385f289683d080be9224bf
Ash Holland <ash@sorrel.sh>
2021-03-16 02:24:57 +0000

    Add Content Security Policy
    Allowing unsafe-inline for styles appears to be required because webpack
    injects CSS at runtime.

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,15 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 module.exports = [
-  new ForkTsCheckerWebpackPlugin()
+  new ForkTsCheckerWebpackPlugin(),
+  new CspHtmlWebpackPlugin({
+    'script-src': "'self'",
+    'style-src': "'self' 'unsafe-inline'",
+  }, {
+    nonceEnabled: {
+      'script-src': false,
+      'style-src': false,
+    },
+  }),
 ];
