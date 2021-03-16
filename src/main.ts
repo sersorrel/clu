95aa0d4ee12541a8e7c905a59256a0576a2a7224
Ash Holland <ash@sorrel.sh>
2021-03-16 00:53:16 +0000

    Only set up devtools in development, not when packaged

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
+const isDev = !app.isPackaged;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -17,21 +19,26 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (isDev) {
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-  installExtension(REACT_DEVELOPER_TOOLS).then(extensions => {
-    console.log(`Installed extensions: ${extensions}`);
-    createWindow();
-  }).catch(err => {
-    console.error(`Failed to install extensions: ${err}`);
-  })
-});
+app.whenReady().then(() => {
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS).then(extensions => {
+      console.log(`Installed extensions: ${extensions}`);
+    }).catch(err => {
+      console.error(`Failed to install extensions: ${err}`);
+    })
+  }
+}).then(() => {
+  createWindow();
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
