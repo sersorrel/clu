8e4cef706a4e02c1d0b8b901acb8d2d21daa7e5a
Ash Holland <ash@sorrel.sh>
2021-03-16 18:15:52 +0000

    Remove script-src self from CSP
    It appears that the integrity attribute is required for hash-based CSP
    on external resources to work.

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,14 +1,18 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlInlineCssWebpackPlugin = require('html-inline-css-webpack-plugin').default;
+const SriPlugin = require('webpack-subresource-integrity');
 const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
   new MiniCssExtractPlugin(),
   new HtmlInlineCssWebpackPlugin(),
+  new SriPlugin({
+    hashFuncNames: ['sha256'],
+  }),
   new CspHtmlWebpackPlugin({
-    'script-src': "'self'",
+    'script-src': "",
     'style-src': "",
   }, {
     nonceEnabled: {
