facb8f03605da4ce1a870c250e5a72086a44f771
Ash Holland <ash@sorrel.sh>
2021-05-09 13:42:55 +0100

    Remove webpack-subresource-integrity
    Now that csp-html-webpack-plugin can add the integrity attribute itself,
    this is no longer necessary.

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -2,7 +2,6 @@ const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlInlineCssWebpackPlugin = require("html-inline-css-webpack-plugin").default;
-const SriPlugin = require("webpack-subresource-integrity");
 const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const isDev = process.env.npm_lifecycle_event === "start";
@@ -16,9 +15,6 @@ module.exports = [
   })] : [],
   new MiniCssExtractPlugin(),
   new HtmlInlineCssWebpackPlugin(),
-  new SriPlugin({
-    hashFuncNames: ["sha256"],
-  }),
   new CspHtmlWebpackPlugin(
     {
       "script-src": isDev ? "'self'" : "",
