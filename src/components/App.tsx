25440b308d7dda8a4e30d5ab7388e5e33bc06809
Ash Holland <ash@sorrel.sh>
2021-05-12 00:27:36 +0100

    Install Redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,23 @@
 import ReactFlow from "react-flow-renderer";
+import { Provider } from "react-redux";
 
 import { Sidebar } from "./Sidebar";
+import { store } from "../store";
 
 import "./App.css";
 
 export function App(): JSX.Element {
-  return <div className="app">
-    <h1 className="app__header">CLU</h1>
-    <Sidebar className="app__sidebar"/>
-    <ReactFlow className="app__graph" elements={[
-      {
-        data: {label: "a node"},
-        id: "1",
-        position: {x: 0, y: 0},
-      },
-    ]}/>
-  </div>;
+  return <Provider store={store}>
+    <div className="app">
+      <h1 className="app__header">CLU</h1>
+      <Sidebar className="app__sidebar"/>
+      <ReactFlow className="app__graph" elements={[
+        {
+          data: {label: "a node"},
+          id: "1",
+          position: {x: 0, y: 0},
+        },
+      ]}/>
+    </div>
+  </Provider>;
 }
