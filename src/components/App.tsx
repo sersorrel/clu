8395189fa4591fa7b5a33de757ea78e5581659a3
Ash Holland <ash@sorrel.sh>
2021-05-11 17:11:18 +0100

    Convert components to named functions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
 import ReactFlow from "react-flow-renderer";
 
-export const App = (): JSX.Element => <>
-  <h1>💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
-  <ReactFlow style={{height: "300px"}} elements={[
-    {
-      data: {label: "a node"},
-      id: "1",
-      position: {x: 0, y: 0},
-    },
-  ]} />
-</>;
+export function App(): JSX.Element {
+  return <>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+    <ReactFlow style={{height: "300px"}} elements={[
+      {
+        data: {label: "a node"},
+        id: "1",
+        position: {x: 0, y: 0},
+      },
+    ]} />
+  </>;
+}
