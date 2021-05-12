5abf4d552b67947bdaf556fc89e0c22ac5242e46
Ash Holland <ash@sorrel.sh>
2021-05-12 22:28:03 +0100

    Use createSlice to create action creators and reducers together
    This makes it harder to forget to write the reducer after writing the
    action creator, or vice versa. It's still necessary to separately define
    and export the action creators, but forgetting to do either of those
    things will lead to a compile-time error, as long as they're actually
    used elsewhere.

--- src/reducers/graph.ts
+++ src/reducers/graph.ts
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuid } from "uuid";
 
 type Command = {
@@ -19,17 +19,6 @@ type Pipe = {
   destinationHandle: string,
 };
 
-export const addCommands = createAction("graph/addCommands", (commands: Array<Omit<Command, "id"> & Partial<Command>>) => {
-  commands.map(command => {
-    command.id ??= uuid();
-  });
-  // TypeScript doesn't seem able to narrow the type of a command all the way to
-  // `Command` even after we give it an `id`.
-  return {payload: commands as Command[]};
-});
-export const removeCommands = createAction<Array<Command["id"]>>("graph/removeCommands");
-export const removePipes = createAction<Array<Pipe["id"]>>("graph/removePipes");
-
 const defaultPipes: Pipe[] = [];
 const defaultCommands: Command[] = [{
   command: "echo 'hello, world!'",
@@ -39,29 +28,44 @@ const defaultCommands: Command[] = [{
   position: {x: 0, y: 0},
 }];
 
-export const graphReducer = createReducer({
-  commands: defaultCommands,
-  pipes: defaultPipes,
-}, builder => {
-  builder
-    .addCase(addCommands, (state, action) => ({
-      ...state,
-      commands: state.commands.concat(action.payload),
-    }))
-    .addCase(removeCommands, (state, action) => {
+const slice = createSlice({
+  initialState: {
+    commands: defaultCommands,
+    pipes: defaultPipes,
+  },
+  name: "graph",
+  reducers: {
+    addCommands: {
+      prepare: (commands: Array<Omit<Command, "id"> & Partial<Command>>) => {
+        commands.map(command => {
+          command.id ??= uuid();
+        });
+        // TypeScript doesn't seem able to narrow the type of a command all the
+        // way to `Command` even after we give it an `id`.
+        return {payload: commands as Command[]};
+      },
+      reducer: (state, action: PayloadAction<Array<Command>>) => ({
+        ...state,
+        commands: state.commands.concat(action.payload),
+      }),
+    },
+    removeCommands(state, action: PayloadAction<Array<Command["id"]>>) {
       const toRemove = new Set(action.payload);
       return {
         ...state,
         commands: state.commands.filter(command => !toRemove.has(command.id)),
         pipes: state.pipes.filter(pipe => !(toRemove.has(pipe.source) || toRemove.has(pipe.destination))),
       };
-    })
-    .addCase(removePipes, (state, action) => {
+    },
+    removePipes(state, action: PayloadAction<Array<Pipe["id"]>>) {
       const toRemove = new Set(action.payload);
       return {
         ...state,
         pipes: state.pipes.filter(pipe => !toRemove.has(pipe.id)),
       };
-    })
-  ;
+    },
+  },
 });
+
+export const { addCommands, removeCommands, removePipes } = slice.actions;
+export const graphReducer = slice.reducer;
