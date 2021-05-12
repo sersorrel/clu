e58ac728a88670c36df3a6f71fd49c56b1f48c87
Ash Holland <ash@sorrel.sh>
2021-05-12 20:19:33 +0100

    Wire up the sidebar to allow drag=and-drop of new nodes

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,7 +1,8 @@
-import ReactFlow, { Edge, Node } from "react-flow-renderer";
+import { useRef } from "react";
+import ReactFlow, { Edge, Node, useZoomPanHelper } from "react-flow-renderer";
 
 import { useDispatch, useSelector } from "../hooks";
-import { removeCommands, removePipes } from "../reducers/graph";
+import { addCommands, removeCommands, removePipes } from "../reducers/graph";
 
 type Props = {
   className?: string,
@@ -21,12 +22,37 @@ export function Graph({className = ""}: Props): JSX.Element {
     target: pipe.destination,
     targetHandle: pipe.destinationHandle,
   }));
-  return <ReactFlow
-    className={className}
-    elements={[...commandElements, ...pipeElements]}
-    onElementsRemove={elements => {
-      dispatch(removePipes(elements.map(element => element.id)));
-      dispatch(removeCommands(elements.map(element => element.id)));
-    }}
-  />;
+
+  const reactFlowWrapper = useRef<HTMLDivElement>(null);
+  const zoomPanHelper = useZoomPanHelper();
+
+  return <div className={className} ref={reactFlowWrapper}>
+    <ReactFlow
+      className={className}
+      elements={[...commandElements, ...pipeElements]}
+      onDragOver={event => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "move";
+      }}
+      onDrop={event => {
+        event.preventDefault();
+        const {command, offset} = JSON.parse(event.dataTransfer.getData("application/x-clu-new-command"));
+        const bounds = reactFlowWrapper.current!.getBoundingClientRect();
+        const position = zoomPanHelper.project({
+          x: event.clientX - bounds.left - offset.x,
+          y: event.clientY - bounds.top - offset.y,
+        });
+        dispatch(addCommands([{
+          command,
+          inputs: 0,
+          outputs: 1,
+          position,
+        }]));
+      }}
+      onElementsRemove={elements => {
+        dispatch(removePipes(elements.map(element => element.id)));
+        dispatch(removeCommands(elements.map(element => element.id)));
+      }}
+    />
+  </div>;
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { DragEvent } from "react";
+
 import "./Sidebar.css";
 
 type Props = {
@@ -5,7 +7,25 @@ type Props = {
 };
 
 export function Sidebar({className = ""}: Props): JSX.Element {
+  const onDragStart = (command: string, event: DragEvent) => {
+    const rect = (event.target as Element).getBoundingClientRect();
+    const offset = {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    };
+    event.dataTransfer.setData("application/x-clu-new-command", JSON.stringify({
+      command,
+      offset,
+    }));
+    event.dataTransfer.effectAllowed = "move";
+  };
   return <div className={`sidebar ${className}`}>
-    <div className="react-flow__node react-flow__node-default sidebar__node">a node</div>
+    <div
+      className="react-flow__node react-flow__node-default sidebar__node"
+      draggable
+      onDragStart={event => onDragStart("echo 'todo'", event)} // TODO
+    >
+      a node
+    </div>
   </div>;
 }
